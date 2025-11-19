2e35a7ad0dedd2cda14f5d449207a64c68b998f8
Lieruce1540 <flint.fabien@laposte.net>
2025-11-19 23:38:39 +0100

    PlacesAPI deprecation update

--- app/src/main/java/com/lieruce/goforlunch/model/Restaurant.java
+++ app/src/main/java/com/lieruce/goforlunch/model/Restaurant.java
@@ -1,21 +1,25 @@
 package com.lieruce.goforlunch.model;
 
+import com.google.android.libraries.places.api.model.PhotoMetadata;
+
+import java.util.List;
+
 public class Restaurant {
 
     private final String id;
     private final String name;
     private final String address;
     private final double rating;
-    private final String photoUrl;
+    private final List<PhotoMetadata> photoMetadatas;
     private final double latitude;
     private final double longitude;
 
-    public Restaurant(String id, String name, String address, double rating, String photoUrl, double latitude, double longitude) {
+    public Restaurant(String id, String name, String address, double rating, List<PhotoMetadata> photoMetadatas, double latitude, double longitude) {
         this.id = id;
         this.name = name;
         this.address = address;
         this.rating = rating;
-        this.photoUrl = photoUrl;
+        this.photoMetadatas = photoMetadatas;
         this.latitude = latitude;
         this.longitude = longitude;
     }
@@ -25,7 +29,7 @@ public class Restaurant {
     public String getName() { return name; }
     public String getAddress() { return address; }
     public double getRating() { return rating; }
-    public String getPhotoUrl() { return photoUrl; }
+    public List<PhotoMetadata> getPhotoMetadatas() { return photoMetadatas; }
     public double getLatitude() { return latitude; }
     public double getLongitude() { return longitude; }
 }
